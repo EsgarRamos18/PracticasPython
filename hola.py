880910798dc7eebbe939b281e741b1f415a58d07
Edgar Ramos <eramoscarrascot@gmail.com>
2022-09-13 10:23:17 -0500

    Proyecto python

---
+++ hola.py
@@ -0,0 +1,7 @@
+name=input("Ingresa tu nombre: ")
+age =input("Ingresa tu edad: ")
+print(f"Hola {name} y tengo {age} años")
+
+future = int(age) + 4
+
+print(f"En el año 2026 tendre {future} años")
