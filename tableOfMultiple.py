b696ed7f368f93818646a074c1084ae6ca70503e
Edgar Ramos <eramoscarrascot@gmail.com>
2022-10-04 09:58:39 -0500

    Tabla de multplicar

--- tableOfMultiple.py
+++ tableOfMultiple.py
@@ -1,4 +1,6 @@
 n1=int(input("Ingresa el numero para crear su tabla de multiplicar: "))
-for n2 in range(1,11):
+n2=int(input("Ingresa el rango: "))
+n2=n2+1
+for n2 in range(1,n2):
 	multiplicacion = n1 * n2 
 	print(f'{n1} x {n2} = {multiplicacion}') 
